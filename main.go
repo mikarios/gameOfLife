14d8a6b7b00827fac70880d1caf8e8cb19dfc871
Manos Ikarios <manos.ikarios.ext@adevinta.com>
2023-11-17 19:31:25 +0200

    a bit of refactoring

--- main.go
+++ main.go
@@ -6,81 +6,59 @@ import (
 	"log"
 	"math/rand"
 	"sync"
-	"time"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-// const width = 1000
-// const height = 1000
-
-var (
-	width  = 640
-	height = 480
-	white  = color.White
-	black  = color.Black
-	blue   = color.RGBA{69, 145, 196, 255}
-	yellow = color.RGBA{255, 230, 120, 255}
-)
+var yellow = color.RGBA{255, 230, 120, 255}
 
 type Game struct {
 	generation uint64
 	grid       [][]bool
 	buffer     [][]bool
+	width      int
+	height     int
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
 	g.generation++
 
-	if rand.Float32() < 0.01 {
-		start := time.Now()
-		defer func(t *time.Time) {
-			fmt.Printf("updated in %v\n", time.Since(*t))
-		}(&start)
-	}
-
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
-		if x <= width-4 && y <= height-4 && x >= 3 && y >= 3 {
+		if x <= g.width-4 && y <= g.height-4 && x >= 3 && y >= 3 {
 			rangeX := []int{-3, -2, -1, 0, 1, 2, 3}
 			rangeY := []int{-3, -2 - 1, 0, 1, 2, 3}
 
 			switch x {
 			case 0:
 				rangeX = []int{0, 1}
-			case width - 1:
+			case g.width - 1:
 				rangeX = []int{-1, 0}
-			case width:
+			case g.width:
 				rangeX = []int{-1}
 			}
 
 			switch y {
 			case 0:
 				rangeY = []int{0, 1}
-			case height - 1:
+			case g.height - 1:
 				rangeY = []int{-1, 0}
-			case height:
+			case g.height:
 				rangeY = []int{-1}
 			}
 
 			for _, xx := range rangeX {
 				for _, yy := range rangeY {
-					if xx == 0 && yy == 0 {
-						g.grid[x][y] = true
-						continue
-					}
-
 					g.grid[x+xx][y+yy] = rand.Float32() < 0.8
 				}
 			}
-
 		}
 	}
 
 	wg := &sync.WaitGroup{}
-	wg.Add(height)
-	for y := 0; y < height; y++ {
+	wg.Add(g.height)
+	for y := 0; y < g.height; y++ {
 		go g.updateX(wg, y)
 	}
 	wg.Wait()
@@ -90,12 +68,12 @@ func (g *Game) Update(screen *ebiten.Image) error {
 	}
 	g.draw(screen)
 
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("Generation: %v", g.generation))
+	_ = ebitenutil.DebugPrint(screen, fmt.Sprintf("Generation: %v", g.generation))
 	return nil
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	if outsideWidth != width || outsideHeight != height {
+	if outsideWidth != g.width || outsideHeight != g.height {
 		log.Println(outsideWidth, outsideHeight)
 		switch {
 		case len(g.grid) > outsideWidth:
@@ -125,17 +103,17 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 			}
 		}
 
-		width, height = outsideWidth, outsideHeight
+		g.width, g.height = outsideWidth, outsideHeight
 	}
 
-	return width, height
+	return g.width, g.height
 }
 
 func (g *Game) draw(window *ebiten.Image) {
-	window.Fill(black)
+	_ = window.Fill(color.Black)
 
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
+	for x := 0; x < g.width; x++ {
+		for y := 0; y < g.height; y++ {
 			if g.grid[x][y] {
 				window.Set(x, y, yellow)
 			}
@@ -146,7 +124,7 @@ func (g *Game) draw(window *ebiten.Image) {
 
 func (g *Game) updateX(ywg *sync.WaitGroup, y int) {
 	defer ywg.Done()
-	for x := 0; x < width; x++ {
+	for x := 0; x < g.width; x++ {
 		g.updatePoint(x, y)
 	}
 }
@@ -159,18 +137,18 @@ func (g *Game) updatePoint(x, y int) {
 	switch x {
 	case 0:
 		rangeX = []int{0, 1}
-	case width - 1:
+	case g.width - 1:
 		rangeX = []int{-1, 0}
-	case width:
+	case g.width:
 		rangeX = []int{-1}
 	}
 
 	switch y {
 	case 0:
 		rangeY = []int{0, 1}
-	case height - 1:
+	case g.height - 1:
 		rangeY = []int{-1, 0}
-	case height:
+	case g.height:
 		rangeY = []int{-1}
 	}
 
@@ -199,17 +177,17 @@ func (g *Game) updatePoint(x, y int) {
 }
 
 func main() {
-	g := &Game{}
-	g.grid = make([][]bool, width)
-	g.buffer = make([][]bool, width)
-	ebiten.SetWindowSize(width, height)
-	ebiten.SetWindowTitle("Your game's title")
+	g := &Game{width: 640, height: 480}
+	g.grid = make([][]bool, g.width)
+	g.buffer = make([][]bool, g.width)
+	ebiten.SetWindowSize(g.width, g.height)
+	ebiten.SetWindowTitle("Game of life")
 	ebiten.SetWindowResizable(true)
 
-	for x := 0; x < width; x++ {
-		g.grid[x] = make([]bool, height)
-		g.buffer[x] = make([]bool, height)
-		for y := 0; y < height; y++ {
+	for x := 0; x < g.width; x++ {
+		g.grid[x] = make([]bool, g.height)
+		g.buffer[x] = make([]bool, g.height)
+		for y := 0; y < g.height; y++ {
 			g.grid[x][y] = rand.Float32() < 0.5
 		}
 	}
